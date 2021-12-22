7558c79c874c24e17f72981109bc8f768e1ba143
Flybird1024 <89476940+Flybird1024@users.noreply.github.com>
2021-12-22 20:53:03 +0800

    Initial commit

---
+++ Two-pointForm.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+//  公式: y-y1 / x-x1 = y2-y1 / x2-x1
+//	- > y(x2-x1) - y1(x2-x1) = x(y2-y1) - x1(y2-y1)
+
+using namespace std;
+
+int main()
+{
+	int x1, y1, x2, y2, Yxishu, integer1, Xxishu, integer2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	Yxishu = x2 - x1;
+	integer1 = y1 * Yxishu;
+	Xxishu = y2 - y1;
+	integer2 = x1 * Xxishu;
+	cout << Yxishu << "y - " << integer1 << " = " << Xxishu << "x - " << integer2;
+}
