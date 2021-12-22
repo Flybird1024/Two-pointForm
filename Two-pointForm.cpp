99199640cc9bbfd9cae7b46e1ac3217dd2c07e10
Flybird1024 <89476940+Flybird1024@users.noreply.github.com>
2021-12-22 22:52:57 +0800

    Two-pointForm_2

--- Two-pointForm.cpp
+++ Two-pointForm.cpp
@@ -7,11 +7,28 @@ using namespace std;
 
 int main()
 {
-	int x1, y1, x2, y2, Yxishu, integer1, Xxishu, integer2;
+	int x1, y1, x2, y2, Yxishu, integer1, Xxishu, integer2, integer3;
+	cout << "輸入x1、y1、x2、y2\n";
 	cin >> x1 >> y1 >> x2 >> y2;
 	Yxishu = x2 - x1;
 	integer1 = y1 * Yxishu;
 	Xxishu = y2 - y1;
 	integer2 = x1 * Xxishu;
-	cout << Yxishu << "y - " << integer1 << " = " << Xxishu << "x - " << integer2;
+	integer3 = integer1 - integer2;
+	if (Xxishu >=0 && Yxishu >= 0)
+		cout << Yxishu << "y - " << Xxishu << "x = " << integer3;
+	else if(Xxishu < 0 && Yxishu >=0) {
+		Xxishu = -1 * Xxishu;
+		cout << Xxishu << "x + " << Yxishu << "y = " << integer3;
+	}
+	else if (Xxishu >= 0 && Yxishu < 0) {
+		Yxishu = -1 * Yxishu;
+		integer3 = -1 * integer3;
+		cout << Xxishu << "x + " << Yxishu << "y = " << integer3;
+	}
+	else if (Xxishu < 0 && Yxishu < 0) {
+		Xxishu = -1 * Xxishu;
+		Yxishu = -1 * Yxishu;
+		cout << Xxishu << "x - " << Yxishu << "y = " << integer3;
+	}
 }
